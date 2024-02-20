5823c592013cd4df299b35dad0b9de97bac39530
barhaking <brhdsylh@gmail.com>
2024-02-20 18:49:16 +0200

    Version 1.0.4
    Changelog:
    * Fixed issue with build
    * Added fully functioning Login Screen (LoginActivity)
    * Added not-fully functioning Rate

--- app/src/main/java/com/example/mathexerciseproject/LoginActivity.java
+++ app/src/main/java/com/example/mathexerciseproject/LoginActivity.java
@@ -1,15 +1,41 @@
 package com.example.mathexerciseproject;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login);
+        initViews();
+        onClickListener();
     }
+
+    public void initViews(){
+        etUsername = findViewById(R.id.etUsername);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    public void onClickListener(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("user", etUsername.getText().toString());
+                startActivity(intent);
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/mathexerciseproject/MainActivity.java
+++ app/src/main/java/com/example/mathexerciseproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mathexerciseproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvNum2;
     private MainViewModel vMain;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +36,13 @@ public class MainActivity extends AppCompatActivity {
         initViews();
         onClickListener();
         models();
+        myIntents();
 
+    }
+    public void myIntents(){
+        Intent intent = getIntent();
+        String userName=intent.getStringExtra("user");
+        setTitle(userName);
     }
 
     public void initViews(){
